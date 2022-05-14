0e54d693d9dd8eea3a8641bf108b3f33a8934659
Daniil Sivakov <daniil.sivakov@effective-soft.com>
2022-05-14 22:27:35 +0300

    Remove StateError to prevent non-fatal

--- lib/notus_format/src/document.dart
+++ lib/notus_format/src/document.dart
@@ -244,11 +244,11 @@ class NotusDocument {
     }
     _delta = _delta.compose(change);
 
-    if (_delta != _root.toDelta()) {
-      throw StateError('Compose produced inconsistent results. '
-          'This is likely due to a bug in the library. Tried to compose change $change from $source.');
+    if (_delta == _root.toDelta()) {
+      _controller.add(NotusChange(before, change, source));
+    } else {
+      // State error (removed to prevent non-fatal)
     }
-    _controller.add(NotusChange(before, change, source));
   }
 
   //
